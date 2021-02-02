effca9a32bbc4b02a0a8bef2b71abd670b6cf309
Aleks-Tl <aleksandr.tloka@i.ua>
2021-02-02 22:31:38 +0200

    finished review screen

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -20,5 +20,40 @@ function testWebP(callback) {
 $(document).ready(function() {
     $('.header__burger, .menu__list, .ovelay').click(function(event) {
         $('.header__burger, .menu__list, .overlay').toggleClass('active');
-    })
+    });
 });
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow_prev.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow_next.png" alt=""></button>',
+        responsive: [
+            {
+              breakpoint: 993,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+              }
+            },
+            {
+              breakpoint: 577,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 320,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+  });
